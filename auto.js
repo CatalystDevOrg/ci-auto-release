f969c93f407817d54cc98421759d67222abdbed7
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-02 16:59:49 +1300

    Update auto.js

--- auto.js
+++ auto.js
@@ -2,9 +2,9 @@
 const { exec } = require("child_process");
 // get time
 const now = new Date();
-const currentMonth = now.getMonth();
+const currentMonth = now.getMonth() + 1;
 const currentYear = now.getFullYear();
-const currentDay = now.getDay();
+const currentDay = now.getDay() + 1;
 
 // execute npm version
 exec(`npm version ${currentYear}.${currentMonth}.${currentDay}-beta`)
